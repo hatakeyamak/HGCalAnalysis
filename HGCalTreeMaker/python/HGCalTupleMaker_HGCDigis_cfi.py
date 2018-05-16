26527d54074d00bd7dbf0c7fe334116bcd9762cc
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2018-05-16 08:23:06 -0500

    add cfi for HGCDigis

---
+++ HGCalTreeMaker/python/HGCalTupleMaker_HGCDigis_cfi.py
@@ -0,0 +1,17 @@
+import FWCore.ParameterSet.Config as cms
+
+hgcalTupleHGCDigis = cms.EDProducer("HGCalTupleMaker_HGCDigis",
+  source = cms.untracked.VInputTag(
+        cms.untracked.InputTag("mix","HGCDigisEE"),
+        cms.untracked.InputTag("mix","HGCDigisHEfront"),
+        cms.untracked.InputTag("mix","HGCDigisHEback")
+        ),
+  geometrySource = cms.untracked.vstring(
+        'HGCalEESensitive',
+        'HGCalHESiliconSensitive',
+        'HCal'
+  ),
+  Prefix = cms.untracked.string  ("HGCDigi"),
+  Suffix = cms.untracked.string  ("")
+)
+
